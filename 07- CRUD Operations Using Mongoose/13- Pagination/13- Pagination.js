3e37567d2898c67588efdb95e77e52ee9b2638ec
Reacher <macryze@icloud.com>
2025-07-08 11:03:25 +0800

    Add example demonstrating pagination using Mongoose.

---
+++ 07- CRUD Operations Using Mongoose/13- Pagination/13- Pagination.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+// Connect to local MongoDB instance
+mongoose
+  .connect("mongodb://localhost/mongodb")
+  .then(() => console.log("Successful Connected..."))
+  .catch((e) => console.log("Fail to Connect...", e));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now() },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function getUser() {
+  const pageNumber = 2;
+  const pageSize = 10;
+
+  const courses = Course.find()
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize);
+  console.log(courses);
+}
+
+getUser();
