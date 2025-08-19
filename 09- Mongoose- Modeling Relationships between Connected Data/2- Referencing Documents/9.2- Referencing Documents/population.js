39762aeaa1d946ddd8e9d55ae8af5e3444f8858d
Reacher <macryze@icloud.com>
2025-08-19 18:44:37 +0800

    Update Mongoose schema: add `author` reference to course model and modify creation function usage

--- 09- Mongoose- Modeling Relationships between Connected Data/2- Referencing Documents/9.2- Referencing Documents/population.js
+++ 09- Mongoose- Modeling Relationships between Connected Data/2- Referencing Documents/9.2- Referencing Documents/population.js
@@ -18,6 +18,10 @@ const Course = mongoose.model(
   "Course",
   new mongoose.Schema({
     name: String,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Author",
+    },
   }),
 );
 
@@ -47,8 +51,8 @@ async function listCourses() {
   console.log(courses);
 }
 
-createAuthor("Mosh", "My bio", "My Website");
+// createAuthor("Mosh", "My bio", "My Website");
 
-// createCourse('Node Course', 'authorId')
+createCourse("Node Course", "68a454641c72847221f8efeb");
 
 // listCourses();
